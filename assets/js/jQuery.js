09c80f44818ba4af4df7f561d7da3ee66d62cb62
Gurubalan-GIT <gurubalan.work@gmail.com>
2019-09-01 20:55:47 +0530

    Commutatus #4 final fix

--- assets/js/jQuery.js
+++ assets/js/jQuery.js
@@ -1,24 +1,3 @@
-$(document).ready(function () {
-    $("#sidebar").mCustomScrollbar({
-        theme: "minimal"
-    });
-    $('#dismiss, .overlay').on('click', function () {
-        $('#sidebar').removeClass('active');
-        $('.overlay').removeClass('active');
-    });
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').addClass('active');
-        $('.overlay').addClass('active');
-        $('.collapse.in').toggleClass('in');
-        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
-    });
-   /* var name=$("#name").val();
-    var email=$("#email").val();
-    var database = firebase.database().ref();
-    database.child(name).set(email);*/
-});
-
-
 $(document).ready(function(){
     $(window).scroll(function () {
            if ($(this).scrollTop() > 50) {
